c04d127383feafd63cd68588787363e22b143687
guptaneha0111 <nehagupta01113@gmail.com>
2022-12-14 17:33:44 +0530

    added files

--- fitnessapp/src/components/Navbar.js
+++ fitnessapp/src/components/Navbar.js
@@ -7,8 +7,10 @@ import Dropdown from "./Dropdown";
 function Navbar()
 {
     const [click, setClick]= useState(false);
+    const [dropdown, setDropdown] = useState(false)
 
     const handleclick = ()=>setClick(!click)
+    const closeMobileMenu = () => setClick(false);
     return(
         <>
         <nav className="navbar">
@@ -17,6 +19,58 @@ function Navbar()
 {/* navbar icon */}
 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
 </div>
+
+<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+         
+          <li
+            className='nav-item' >
+            <Link
+              to='/products'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Products <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+
+          <li className='nav-item'>
+            <Link
+              to='/support'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Support
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link
+              to='/blog'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Blog
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link to='/forums' className='nav-links' onClick={closeMobileMenu}>
+              Forums
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to='/about'
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+        <Button />
         </nav>
         </>
     )
